fabab1fed37c430a0fb11b49ad8c238ca1077f6a
sadcof <sadsidecoff@gmail.com>
2023-04-13 17:51:26 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,17 +11,17 @@ namespace Store
         static void Main(string[] args)
         {
             int ownGold;
-            int crystals;
+            int crystalsPiece;
             int crystalscost = 15;
 
             Console.WriteLine($"Добро пожаловать в магазин кристаллов! Один кристалл стоит {crystalscost} голды.");
             Console.Write("Ведите ваше количество голды: ");
             ownGold = Convert.ToInt32(Console.ReadLine());
             Console.Write("Введите количество кристаллов, которое хотите купить: ");
-            crystals = Convert.ToInt32(Console.ReadLine());
+            crystalsPiece = Convert.ToInt32(Console.ReadLine());
 
-            ownGold -= crystals * crystalscost;
-            Console.WriteLine($"У вас осталось голды: {ownGold}; вы купили кристаллов: {crystals} шт");
+            ownGold -= crystalsPiece * crystalscost;
+            Console.WriteLine($"У вас осталось голды: {ownGold}; вы купили кристаллов: {crystalsPiece} шт");
 
             Console.ReadKey();
         }
